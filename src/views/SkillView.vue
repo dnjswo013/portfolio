7f13c34e9fb41d3554393b248d245a19d00e792f
Kim Jae Won <dnjswo013@gmail.com>
2022-09-18 17:48:31 +0900

    update skill page and multiple card

---
+++ src/views/SkillView.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="wrapper">
+    <div class="card">
+      <img src="../assets/ahoudori.jpeg" alt="Avatar" style="width: 100%" />
+      <div class="container">
+        <h4><b>John Doe</b></h4>
+        <p>Architect & Engineer</p>
+      </div>
+    </div>
+    <div class="card">
+      <img src="../assets/ahoudori.jpeg" alt="Avatar" style="width: 100%" />
+      <div class="container">
+        <h4><b>John Doe</b></h4>
+        <p>Architect & Engineer</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.card {
+  /* Add shadows to create the "card" effect */
+  box-shadow: 4px 4px 8px 4px rgba(0, 0, 0, 0.2);
+  transition: 0.5s;
+  margin: auto;
+}
+
+/* On mouse-over, add a deeper shadow */
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+}
+
+/* Add some padding inside the card container */
+.container {
+  padding: 2px 16px;
+}
+</style>
