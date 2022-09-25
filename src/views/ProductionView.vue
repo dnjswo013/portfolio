8bc85315ac3384ad97013478957ecb70edbec63c
Kim Jae Won <dnjswo013@gmail.com>
2022-09-25 21:31:02 +0900

    update skill sheet and production sheet

---
+++ src/views/ProductionView.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { reactive } from "vue";
+import { Production } from "@/interface/production";
+
+const skill_list = reactive<Production[]>([
+  {
+    name: "Production",
+    image: require("../assets/ahoudori.jpeg"),
+    content: "Full Stack Engineering",
+  },
+]);
+</script>
+
+<template>
+  <div class="header">Productions</div>
+  <div class="card_group">
+    <div class="card" v-for="skill in skill_list" :key="skill.name">
+      <!-- <img class="card__imgframe" :src="skill.image" /> -->
+      <div class="description">
+        <h4>{{ skill.name }}</h4>
+        <p>{{ skill.content }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style src="../style/card.css" scoped></style>
